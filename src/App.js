cbb9b518c4405de12af3e1fd0572a1284062c9e5
beckaone <beckaone83@gmail.com>
2021-09-30 14:32:29 +0200

    Rechner entschlackt

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
 import "./App.css";
 import RechnerKlasse from "./Rechner.js";
+import { useState } from "react";
 
 function App() {
   const rechner = new RechnerKlasse();
 
-  const addition = () => {
-    rechner.onAddition();
-  };
+
+  const [wert1, setWert1] = useState("");
+  const [wert2, setWert2] = useState("");
+  const [ergebnis, setErgebnis] = useState("");
+  const [zeigeWarnung, setZeigeWarnung] = useState(false)
+
 
   return (
     <div className="App">
       <h1>Benny's Rechner</h1>
 
       <form name="formular">
-        <input id="F1" type="text" size="7"></input>
-        <input id="F2" type="text" size="7"></input>
+        <input id="F1" type="text" size="7" value={wert1} onChange={(event) => setWert1(event.target.value)}></input>
+        <input id="F2" type="text" size="7" value={wert2} onChange={(event) => setWert2(event.target.value)}></input>
 
-        <button type="button" onClick={addition}>
+        <button type="button" onClick={() => {setErgebnis(rechner.onAddition(wert1, wert2))
+                                              setZeigeWarnung(false)}}>
           +
         </button>
-        <button type="button" onClick={rechner.onSubtraktion}>
+        <button type="button" onClick={() => setErgebnis(rechner.onSubtraktion(wert1, wert2))}>
           -
         </button>
-        <button type="button" onClick={rechner.onMultiplikation}>
+        <button type="button" onClick={() => setErgebnis(rechner.onMultiplikation(wert1, wert2))}>
           *
         </button>
-        <button type="button" onClick={rechner.onDivision}>
+        <button type="button" onClick={() => setErgebnis(rechner.onDivision(wert1, wert2))}>
           /
         </button>
-        <button type="button" onClick={rechner.onProzent}>
+        <button type="button" onClick={() => setErgebnis(rechner.onProzent(wert1, wert2))}>
           in %
         </button>
-        <button type="button" onClick={rechner.onQuadratwurzel}>
+        <button type="button" onClick={() => {
+                                              if (wert2) {
+                                              setZeigeWarnung(true); 
+                                              } 
+                                              setErgebnis(rechner.onQuadratwurzel(wert1))}}>
           √
         </button>
-        <button type="button" onClick={rechner.onQuadrat}>
+        <button type="button" onClick={() => {
+                                              if (wert2) {
+                                              alert('Es wird nur mit Eingabefeld 1 gerechnet!')
+                                              } 
+                                              setErgebnis(rechner.onQuadrat(wert1))}}>
           x²
         </button>
+        
 
-        <input id="F3" type="text" size="20"></input>
+        <input id="F3" type="text" size="7" defaultValue={ergebnis}></input>
       </form>
+      {zeigeWarnung && <p>Es wird nur mit Eingabefeld 1 gerechnet!</p>}
+      
     </div>
   );
 }
--- src/Rechner.js
+++ src/Rechner.js
@@ -1,67 +1,5 @@
 export default class RechnerKlasse {
-  onAddition() {
-    const fieldValue = document.getElementById("F1").value;
-    let intValue = parseInt(fieldValue);
-    const field2Value = document.getElementById("F2").value;
-    let intValue2 = parseInt(field2Value);
-
-    this.onAdditionNachricht(intValue, intValue2);
-  }
-  onAdditionNachricht(field1, field2) {
-    document.getElementById("F3").value = `${field1} + ${field2} = ${
-      field1 + field2
-    }`;
-  }
-
-  onSubtraktion() {
-    const fieldValue = document.getElementById("F1").value;
-    let intValue = parseInt(fieldValue);
-    const field2Value = document.getElementById("F2").value;
-    let intValue2 = parseInt(field2Value);
-    this.onSubtraktionNachricht(intValue, intValue2);
-  }
-  onSubtraktionNachricht(field1, field2) {
-    document.getElementById("F3").value = `${field1} - ${field2} = ${
-      field1 - field2
-    }`;
-  }
-
-  onMultiplikation() {
-    const fieldValue = document.getElementById("F1").value;
-    let intValue = parseInt(fieldValue);
-    const field2Value = document.getElementById("F2").value;
-    let intValue2 = parseInt(field2Value);
-    this.onMultiplikationNachricht(intValue, intValue2);
-  }
-  onMultiplikationNachricht(field1, field2) {
-    document.getElementById("F3").value = `${field1} * ${field2} = ${
-      field1 * field2
-    }`;
-  }
-
-  onDivision() {
-    const fieldValue = document.getElementById("F1").value;
-    let intValue = parseInt(fieldValue);
-    const field2Value = document.getElementById("F2").value;
-    let intValue2 = parseInt(field2Value);
-    this.onDivisionNachricht(intValue, intValue2);
-  }
-  onDivisionNachricht(field1, field2) {
-    document.getElementById("F3").value = `${field1} / ${field2} = ${this.round(
-      field1 / field2
-    )}`;
-  }
-
-  onQuadratwurzel() {
-    const fieldValue = document.getElementById("F1").value;
-    let intValue = parseInt(fieldValue);
-    if (document.getElementById("F2").value) {
-      alert(
-        "Für das Ziehen der Quadratwurzel wird nur der Wert aus dem ersten Eingabefeld verwendet"
-      );
-    }
-    this.onQuadratwurzelNachricht(intValue);
-  }
+  
 
   round(wert) {
     wert = parseFloat(wert);
@@ -71,38 +9,35 @@ export default class RechnerKlasse {
     const ergebnis = wert * 100;
     return Math.round(ergebnis) / 100;
   }
-  onQuadratwurzelNachricht(field1) {
-    document.getElementById("F3").value = `√${field1} = ${this.round(
-      Math.sqrt(field1)
-    )}`;
+
+  
+  
+  onAddition(wert1, wert2) {
+    return parseFloat(wert1) + parseFloat(wert2);
+  }
+  
+  onSubtraktion(wert1, wert2) {
+    return parseFloat(wert1) - parseFloat(wert2);
   }
 
-  onQuadrat() {
-    const fieldValue = document.getElementById("F1").value;
-    let intValue = parseInt(fieldValue);
-    if (document.getElementById("F2").value) {
-      alert(
-        "Um die Quadratzahl auszurechnen wird nur der Wert aus dem ersten Eingabefeld verwendet"
-      );
-    }
-    this.onQuadratNachricht(intValue);
-  }
-  onQuadratNachricht(field1) {
-    document.getElementById("F3").value = `${field1}² = ${field1 * field1}`;
+  onMultiplikation(wert1, wert2) {
+    return parseFloat(wert1) * parseFloat(wert2);
   }
 
-  onProzent() {
-    const fieldValue = document.getElementById("F1").value;
-    let intValue = parseInt(fieldValue);
-    const field2Value = document.getElementById("F2").value;
-    let intValue2 = parseInt(field2Value);
-    this.onProzentNachricht(intValue, intValue2);
+  onDivision(wert1, wert2) {
+    return this.round(parseFloat(wert1) / parseFloat(wert2));
   }
-  onProzentNachricht(field1, field2) {
-    document.getElementById(
-      "F3"
-    ).value = `${field1} von ${field2} = ${this.round(
-      (field1 / field2) * 100
-    )}%`;
+
+  onQuadratwurzel(wert1) {
+    return this.round(Math.sqrt(parseFloat(wert1)));
   }
+
+  onQuadrat(wert1) {
+    return Math.pow(parseFloat(wert1), 2);
+  }
+
+  onProzent(wert1, wert2) {
+    return this.round(parseFloat(wert1) / parseFloat(wert2) * 100);
+  }
+  
 }
